3b30fac1cc8dd7681a7292eb61b9d82c1bc347d8
Artem Balashov <pockerhead@ya.ru>
2020-12-09 13:05:38 +0300

    Initial Commit

---
+++ SwiftUIPerfomanceTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUIPerfomanceTestApp
+//
+//  Created by artem on 09.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
